798379b9558c0a3a840f50cc7a369b917278220c
Giedo Stolk <giedo.stolk@ns.nl>
2024-10-17 11:38:56 +0200

    AWS tools

--- install_awscli.sh
+++ install_awscli.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 sudo apt install curl unzip
-mkdir -p /tmp/install
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/install/awscliv2.zip"
 unzip /tmp/install/awscliv2.zip
-sudo bash /tmp/install/aws/install
+sudo ./aws/install
